a104c56d712135d428f05442446fe770493a51e4
root <root@ip-10-1-1-126.ec2.internal>
2025-05-07 13:47:54 +0000

    modified SG ingress and added egress

--- 5.security_groups.tf
+++ 5.security_groups.tf
@@ -9,7 +9,21 @@ resource "aws_security_group" "allow_all_traffic" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
+ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["117.213.151.238/32"]
+  }
+
+ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["117.213.151.238/32"]
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
