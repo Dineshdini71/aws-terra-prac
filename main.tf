2faa4216be96d6adb00df4b966d9f18546ac41f9
Dineshdini71 <dinidinesh71@gmail.com>
2025-05-10 21:19:37 +0530

    Workspace code

---
+++ main.tf
@@ -0,0 +1,135 @@
+#This Terraform Code Deploys Basic VPC Infra.
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_version = "<= 1.11.4"
+  required_providers {
+    aws = {
+      version = "<= 5.0.0"
+      source  = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-tfstate-buck8"
+    key    = "workspaces.tfstate"
+    region = "us-east-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terra_state_lock"
+    encrypt        = true
+  }
+}
+
+resource "aws_vpc" "default" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name        = "${var.vpc_name}"
+    Owner       = "Dinesh"
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+  tags = {
+    Name = "${var.IGW_name}"
+  }
+}
+
+resource "aws_subnet" "subnet1-public" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet1_cidr
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "${var.public_subnet1_name}"
+  }
+}
+
+resource "aws_subnet" "subnet2-public" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet2_cidr
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "${var.public_subnet2_name}"
+  }
+}
+
+resource "aws_subnet" "subnet3-public" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet3_cidr
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "${var.public_subnet3_name}"
+  }
+
+}
+
+
+resource "aws_route_table" "terraform-public" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+
+  tags = {
+    Name = "${var.Main_Routing_Table}"
+  }
+}
+
+resource "aws_route_table_association" "terraform-public" {
+  subnet_id      = aws_subnet.subnet1-public.id
+  route_table_id = aws_route_table.terraform-public.id
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "web-1" {
+  ami                         = "ami-084568db4383264d4"
+  availability_zone           = "us-east-1a"
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  subnet_id                   = aws_subnet.subnet1-public.id
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  associate_public_ip_address = true
+  tags = {
+    Name       = "${var.env}-webServer-1"
+    Env        = "Prod"
+    Owner      = "Dini"
+    CostCenter = "ABCD"
+  }
+  user_data = <<-EOF
+        #!/bin/bash
+        sudo apt update && apt install -y
+        sudo apt install nginx -y
+        sudo systemctl start nginx
+        sudo systemctl enable nginx
+        echo "<center><h1 style="color:organered;">${var.env}-webServer-1<h1></center>" | sudo tee /var/www/html/index.nginx-debian.html
+    EOF 
+}
+
